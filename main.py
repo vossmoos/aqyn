4d6fe6bfcb8b2dd1ec20168b7140dbc8bf816d1b
Andrey Olishchuk <bobroe.web@gmail.com>
2024-08-12 20:29:04 +0200

    ask a question, embed search combined

--- main.py
+++ main.py
@@ -135,10 +135,10 @@ async def ask_question(question: Question):
         collection = chroma.get_collection(question.tenant)
         emb = gemini.get_embed(question.text[:MAX_CHARS])
         documents = chroma.answer(collection, emb[0].values)
-        # doc_string = ", ".join(documents["documents"])
+        doc_string = ", ".join(documents["documents"][0])
         return {
             "question": question.text,
-            "answer": documents["documents"]
+            "answer": doc_string
         }
     except Exception as e:
         return {
